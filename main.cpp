1c7945df1ba5f234c2185ac63484ddcef3e94dc9
Oliver Jørgensen <anizto190196@gmail.com>
2024-11-19 09:15:29 +0100

    Added rayib

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
+#include "raylib.h"
 
+int main()
+{
+    InitWindow(800, 450, "raylib [core] example - basic window");
 
-int main() {
-	return 0;
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
